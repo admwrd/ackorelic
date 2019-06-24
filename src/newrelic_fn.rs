a7c13f965afeecd094ada9a6e5865858092f2c25
Ankit Das <ankit.das@acko.tech>
2019-06-24 14:45:16 +0530

    made code compilable

--- src/newrelic_fn.rs
+++ src/newrelic_fn.rs
@@ -9,9 +9,7 @@ use crate::segment::Segment;
         .web_transaction("init")
         .expect("Could not start transaction"));
 
-    pub static TL_SEGMENT: RefCell<Segment + 'static> = RefCell::new(Segment::custom( TL_TRANSACTION.with(|tr| {
-        *tr.borrow_mut()
-    });, "init","Custom"));
+    //pub static TL_SEGMENT: RefCell<Segment> = RefCell::new(Segment::custom( TL_TRANSACTION.with(|tr| {*tr.borrow_mut()}), "init","Custom"));
 }
 
 pub fn nr_start_web_transaction(name: &str) -> () {
@@ -34,17 +32,17 @@ pub fn nr_end_transaction() {
 
 pub fn nr_start_custom_segment(name: &str) -> () {
     println!("Starting custom segment name : {}", name);
-    let seg = TL_TRANSACTION.with(|tr| {
-        Segment::custom(&tr.borrow_mut(), name,"Custom")
-    });
-    TL_SEGMENT.with(|segment| {
-        *segment.borrow_mut() = seg;
-    });
+//    let seg = TL_TRANSACTION.with(|tr| {
+//        Segment::custom(&tr.borrow_mut(), name,"Custom")
+//    });
+//    TL_SEGMENT.with(|segment| {
+//        *segment.borrow_mut() = seg;
+//    });
 }
 
 pub fn nr_end_custom_segment() -> () {
     println!("Ending custom segment");
-    TL_SEGMENT.with(|seg| {
-        seg.borrow_mut().end();
-    });
+//    TL_SEGMENT.with(|seg| {
+//        seg.borrow_mut().end();
+//    });
 }
