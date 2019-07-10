692bf89a8c62ae6767ff692ac46dad32c9c5f0b6
ankitdas <50612736+ankitdas@users.noreply.github.com>
2019-07-10 14:55:35 +0530

    removed key

--- src/nr_init.rs
+++ src/nr_init.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 
 lazy_static! {
     pub static ref NR_APP: App = {
-        let license_key = env::var("NEW_RELIC_LICENSE_KEY").unwrap_or_else(|_| "df9713ab7d366e521d6b4a29b1971ee45b8415c4".to_string());
+        let license_key = env::var("NEW_RELIC_LICENSE_KEY").unwrap_or_else(|_| "".to_string());
         let app_name = env::var("NEW_RELIC_APP_NAME").unwrap_or_else(|_| "acko_api_test".to_string());
         let app = App::new(&app_name, &license_key).expect("Could not create app");
         app
